0d3f3ae830e3b3fbfcd042fecc4d773118bf635c
Vinay kumar <148427084+vinaykumar0410@users.noreply.github.com>
2024-01-09 12:17:45 +0530

    Add files via upload

---
+++ trees/leetcode-938.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    // initialize sum as global variable as we use recursion
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        // traverse tree until root becomes null
+        if(root != null){
+            // call left subtree
+            rangeSumBST(root.left,low,high);
+            // check for base condition for adding val to sum
+            if(root.val >= low && root.val <= high) sum += root.val;
+            // call right subtree
+            rangeSumBST(root.right,low,high);
+        }
+        // return the sum which holds all the tree vals that meets condition
+        return sum;
+    }
+}
